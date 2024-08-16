f5860dfcd9252465f153f9e31a1ba37fce69fe6d
nayeamseikhh <usanayeamseikh@gmail.com>
2024-08-16 12:30:20 +0600

    2nd

---
+++ null-vs-undefined.js
@@ -0,0 +1,25 @@
+// vafiable fully not decleared
+let pakhi;
+console.log(pakhi);
+// no return;
+function add(num1, num2){
+    console.log(num1, num2);
+}
+const result = add( 82 );  //not set num2(seceond value)
+console.log(result);
+
+
+function added(number1, number2){
+    console.log(number1, number2);
+    return; //fully not return
+}
+const results = added( 84, 4 );
+console.log(results);
+
+// find undefine value
+const premil = { name:"jhon", phone: "4453543"};
+console.log(premil.gf);
+
+
+
+
---
+++ true-false.js
@@ -0,0 +1,21 @@
+//Falsy:
+// false;
+//0;
+//"";
+// undefined;
+// null;
+// NaN;
+//
+
+//Truthy:
+// '0'. ' ' [];
+//"fasle";
+//'true';
+
+let name = 12;
+if(name  || name == 0){
+    console.log("consition is true");
+}
+else{
+    console.log("condition is False");
+}
